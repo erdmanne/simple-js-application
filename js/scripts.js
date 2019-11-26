0f19d207ebcfa99a7a8a62eddeca07ffe2bda9e1
erdmanne <hello@digital4small.de>
2019-11-26 10:28:53 +0100

    finish task 1.1 JS

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+alert()
+
+var myName = 'Bob';
+document.write(myName);
+var myName = 'Bob';
+document.write(myName);
+
+var favfood ="Pasta";
+document.write (favfood)
+
+
+
+
+
+
+/* var myName = 'Fred';
+alert(myName);
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+var size = 100;
+var doubleSize = size * 2;
+document.write(doubleSize);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize); */
