0c4d1af651cdab4da91b1bba47dce4fbd4826fdb
erdmanne <hello@digital4small.de>
2019-11-26 12:51:14 +0100

    added initial pokemon data
    for task 1.2 js for CF

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,23 @@
-alert()
-
-var myName = 'Bob';
-document.write(myName);
-var myName = 'Bob';
-document.write(myName);
-
-var favfood ="Pasta";
-document.write (favfood)
-
-
-
-
-
-
-/* var myName = 'Fred';
-alert(myName);
-
-var simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-var size = 100;
-var doubleSize = size * 2;
-document.write(doubleSize);
-
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize); */
+repository = [];
+repository = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: 0.8,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Venusaur",
+    height: 0.9,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Clefable",
+    height: 0.4,
+    types: ["fairy"],
+  },
+]
