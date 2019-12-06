6676f79bb2f239f0aad5aa6184d41a55252a07d3
erdmanne <hello@digital4small.de>
2019-12-06 11:36:19 +0100

    added "forEach" function for Task 1.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
-var repository = []
-repository = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charmander",
-    height: 0.8,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Venusaur",
-    height: 1.9,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Clefable",
-    height: 0.4,
-    types: ["fairy"],
-  },
-]
-//loop for task 1.3 JS
+
+var repository = [
+{name: 'Bulbasaur', height: 7, type:['grass', ' poison']},
+{name: 'Charizard', height: 5, type:['fire', ' flying']},
+{name: 'Butterfree', height: 3, type:['bug', ' flying']},
+];
+
+
+ /* Applied For Each Loop To Pokedex Array */
+ repository.forEach(function(property) { /* Applied For Each Loop To Pokedex Array */
+   document.write("<br>"+"Name: "+property.name + "  | Height: "+property.height+" | Type: "+property.type);
+   if(property.height > 5){
+      document.write("  (- Wow that is a big Pokemon!)")
+ }
+ document.write("<p>");
+ });
+
+
+/*loop for task 1.3 JS
 for (items in repository) {
   document.write(repository[items].name + ' (height: ' + repository[items].height + 'm)<br>');
   if (repository[items].height > 1) {
@@ -30,3 +25,13 @@ for (items in repository) {
     document.write(repository[items].name + ' (height: ' + repository[items].height + 'm)<br><br>');
   }
 }
+
+// further explantion: https://www.youtube.com/watch?v=W6NZfCO5SIk
+let name = 'eva';
+console.log (name);
+
+function greet(name) {
+  console.log ('hello ' + name);
+}
+greet ('Eva');
+*/
