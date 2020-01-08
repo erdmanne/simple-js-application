9dda8489ee60a72c21d421fa7a49c9aa0f402f9b
Eva Erdmann <erdmanne@mediamarktsaturn.com>
2020-01-08 11:38:16 +0100

    task 1.5
    many struggles during a laptopchange :( sorry for no better explanations

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,100 @@
 
-var repository = [
-{name: 'Bulbasaur', height: 7, type:['grass', ' poison']},
-{name: 'Charizard', height: 5, type:['fire', ' flying']},
-{name: 'Butterfree', height: 3, type:['bug', ' flying']},
-];
+/*!
+* Here comes all Javascript
+* Pokedoki App
+* Date: 1.5 task
+*/
 
+// wrap repository array in an IIFE to avoid accidentally accessing the global state.
+// create a new pokemonRepository variable
+var pokemonRepository = (function () {
+  var repository = [];
+  // This array will become the repository of Pokémon to display in myapplication
 
- /* Applied For Each Loop To Pokedex Array */
- repository.forEach(function(property) { /* Applied For Each Loop To Pokedex Array */
-   document.write("<br>"+"Name: "+property.name + "  | Height: "+property.height+" | Type: "+property.type);
-   if(property.height > 5){
-      document.write("  (- Wow that is a big Pokemon!)")
- }
- document.write("<p>");
- });
-
-
-/*loop for task 1.3 JS
-for (items in repository) {
-  document.write(repository[items].name + ' (height: ' + repository[items].height + 'm)<br>');
-  if (repository[items].height > 1) {
-    document.write(repository[items].name + " (height: " + repository[items].height + "m) - Wow, that's big!<br><br>");
-  } else {
-    document.write(repository[items].name + ' (height: ' + repository[items].height + 'm)<br><br>');
+  function add(pokemon) {
+    repository.push(pokemon);
   }
-}
 
-// further explantion: https://www.youtube.com/watch?v=W6NZfCO5SIk
+  function getAll() {
+    return repository;
+  }
+
+  function addListItem(pokemon) {
+    var $pokemonList = document.querySelector("ul");
+    var $listItem = document.createElement("li");
+    var $button = document.createElement("button");
+    $pokemonList.appendChild($listItem);
+    $listItem.appendChild($button);
+    $button.innerText = pokemon.name;
+    $button.classList.add("list-button");
+    $listItem.classList.add("buttonstyle");
+    $button.addEventListener("click", function(event) {
+    showDetails(pokemon);
+    });
+  }
+
+  //Function to show details of each Pokemon
+    function showDetails(pokemon) {
+       console.log(pokemon.name + pokemon.height + pokemon.type);
+       alert(pokemon.name + pokemon.height + pokemon.type);
+     }
+
+  return { /*Return All Previous Function In Order To Be Available Outside Of IIFE */
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
+  };
+
+})();
+
+var absol = {
+  name:'Absol',
+  height: 1.2,
+  type: 'dark',
+};
+
+var luxray = {
+  name:'Luxray',
+  height: 1.4,
+  type: 'electric',
+};
+
+var pikachu = {
+  name:'Pikachu',
+  height: 0.4,
+  type: 'electric',
+};
+
+var milotic = {
+  name: 'Milotic',
+  height: 6.2,
+  type: 'water',
+};
+
+var eevee = {
+  name:'Eevee',
+  height: 0.3,
+  type: 'normal',
+};
+
+
+pokemonRepository.add(absol);
+pokemonRepository.add(luxray);
+pokemonRepository.add(pikachu);
+pokemonRepository.add(milotic);
+pokemonRepository.add(eevee);
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
+
+
+
+
+/*
+further explantion: https://www.youtube.com/watch?v=W6NZfCO5SIk
 let name = 'eva';
 console.log (name);
 
